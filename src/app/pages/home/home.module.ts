2c948a15c53f9ff9c501ce7afe72004c7d64c43a
Paulo <pauloafpjunior@hotmail.com>
2019-09-06 13:37:35 -0300

    limpando arquivos desnecessários

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -6,7 +6,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { HomePage } from './home.page';
-import { PipesModule } from 'src/app/modules/pipes/pipes.module';
 import { SharedModule } from 'src/app/modules/shared/shared.module';
 
 const routes: Routes = [
@@ -26,4 +25,4 @@ const routes: Routes = [
   ],
   declarations: [HomePage]
 })
-export class HomePageModule {}
+export class HomePageModule { }
